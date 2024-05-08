5bb6af51bec9f478336d0eb49bff74ecdb8db8a7
Andrew Plotkin <zarf@ZarfLent.local>
2024-05-07 21:38:43 -0400

    Get the images out there.

--- js/phogg.js
+++ js/phogg.js
@@ -1,8 +1,49 @@
 'use strict';
 
+var imagesize = 190;
+
+function build_pic_el(pic)
+{
+    var cellel = $('<div>', { class:'PhotoCell' });
+    cellel.append($('<div>', { class:'Filename' }).text(pic.pathname));
+    var outel = $('<a>', { href:'testpics/'+pic.pathname, target:'_blank' }).text('\u21D7');
+    cellel.append($('<div>', { class:'OutButton' }).append(outel));
+
+    var width, height;
+    if (pic.aspect > 1) {
+	width = imagesize;
+	height = Math.floor(imagesize / pic.aspect);
+    }
+    else {
+	height = imagesize;
+	width = Math.floor(imagesize * pic.aspect);
+    }
+    
+    var imgel = $('<img>', { class:'Photo', loading:'lazy', src:'testpics/'+pic.pathname, width:width, height:height });
+    cellel.append(imgel);
+    
+    var boxel = $('<div>', { class:'PhotoCellBox' });
+    boxel.append($('<div>', { class:'PhotoCellGap' }));
+    boxel.append(cellel);
+    boxel.append($('<div>', { class:'PhotoCellGap' }));
+
+    return boxel;
+}
+
 function evhan_api_getpics(data, status, jqreq)
 {
     console.log('### request success', status, data);
+    if (data.pics) {
+	for (var pic of data.pics) {
+	    pic.aspect = pic.width / pic.height;
+	}
+	
+	var parel = $('.PhotoGrid');
+	parel.empty();
+	for (var pic of data.pics) {
+	    parel.append(build_pic_el(pic));
+	}
+    }
 }
 
 function evhan_api_error(jqreq, status, error)
