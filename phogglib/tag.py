cc16968550266c1ae8af146b1f0c05712a667b28
Andrew Plotkin <zarf@ZarfLent.local>
2024-06-06 19:00:06 -0400

    Add a sluggifier.

--- phogglib/tag.py
+++ phogglib/tag.py
@@ -1,3 +1,4 @@
+import re
 
 class Tag:
     def __init__(self, tag, autogen):
@@ -11,3 +12,27 @@ class Tag:
         }
         return res
     
+pat_basictag = re.compile('^[a-zA-Z0-9: -]*$')
+pat_fancychar = re.compile('[^a-zA-Z0-9:-]')
+
+def escapefancy(match):
+    ch = match.group(0)
+    if ch == ' ':
+        return '_'
+    return '=%X=' % (ord(ch),)
+
+def tagfilename(val):
+    """Turn any string into an ASCII equivalent which can be used as a
+    filename. (We don't want to worry about whether the filesystem supports
+    UTF-8.)
+
+    Different strings must always map to different strings; after that,
+    human readability is nice. We don't have to reverse this mapping.
+    """
+    if not val:
+        return '=='
+    if pat_basictag.match(val):
+        return val.replace(' ', '_')
+    return pat_fancychar.sub(escapefancy, val)
+
+
